15fcc31a74c00116753efc021bc84a20af38d165
Luisillo SavaggeX <144190931+LuisilloSavagge@users.noreply.github.com>
2023-09-13 19:04:54 -0600

    Add files via upload

---
+++ p10_1s22.java
@@ -0,0 +1,9 @@
+public class P10_1s22{
+	public static void main(String[] arg) {
+
+    if (true){
+      System.out.println("por que aparezco...");
+    }
+    System.out.println("que paso ...");
+  }
+}
